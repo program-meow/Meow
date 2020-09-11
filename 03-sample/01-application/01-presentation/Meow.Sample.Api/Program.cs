4f4f82ea791725a68edfe09e5c216d9432af23ee
meow <work@program-meow.com>
2020-09-11 21:56:29 +0800

    强迫症改下文件名称大小写

---
+++ 03-sample/01-application/01-presentation/Meow.Sample.Api/Program.cs
@@ -0,0 +1,44 @@
+using Meow.Exception;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Meow.Sample.Api
+{
+    /// <summary>
+    /// 应用程序
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// 应用程序入口点
+        /// </summary>
+        /// <param name="args">入口点参数</param>
+        public static void Main(string[] args)
+        {
+            try
+            {
+                WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>()
+                    .Build()
+                    .Run();
+            }
+            catch (System.Exception ex)
+            {
+                throw new Warning($"应用程序启动失败:{ex.Message}");
+            }
+            //CreateHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// 创建主机生成器
+        /// </summary>
+        /// <param name="args">入口点参数</param>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ 03-sample/01-application/04-infrastructure/Meow.Sample.Data/Repository/Systems/ApplicationRepository.cs
@@ -0,0 +1,21 @@
+﻿using Meow.Data.Ef;
+using Meow.Sample.Data.UnitOfWork;
+using Meow.Sample.Domain.Model;
+using Meow.Sample.Domain.Repository;
+
+namespace Meow.Sample.Data.Repository.Systems
+{
+    /// <summary>
+    /// 应用程序仓储
+    /// </summary>
+    public class ApplicationRepository : CrudRepository<Application>, IApplicationRepository
+    {
+        /// <summary>
+        /// 初始化应用程序仓储
+        /// </summary>
+        /// <param name="unitOfWork">工作单元</param>
+        public ApplicationRepository(ISampleUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
