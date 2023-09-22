e9cc374bc7029474a9cea9d1c4c16019b1e1dc88
程序喵 <resource@program-meow.com>
2023-09-23 00:07:39 +0800

    升级

--- test/Meow.Common.Test/Function/FunctionError.cs
+++ test/Meow.Common.Test/Function/FunctionError.cs
@@ -1,26 +1,21 @@
 ﻿using Xunit.Sdk;
 
-namespace Meow.Common.Test.Function
-{
+namespace Meow.Common.Test.Function {
     /// <summary>
     /// 错误函数
     /// </summary>
-    public static class FunctionError
-    {
+    public static class FunctionError {
         /// <summary>
         /// 无返回值
         /// </summary>
-        public static void Void()
-        {
-            throw new NotNullException();
+        public static void Void() {
         }
 
         /// <summary>
         /// 返Bool
         /// </summary>
-        public static bool ReturnBool()
-        {
-            throw new NotNullException();
+        public static bool ReturnBool() {
+            return false;
         }
     }
 }
